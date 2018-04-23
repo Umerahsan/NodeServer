51e3d574720b35142bb81e0c7d2bfd7542579d12
Umerahsan <ahsan.umer@tftus.com>
2018-04-23 12:43:49 +0530

    re

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express();
 
-const morgan = require('morgan')
-const productRoutes = require('./api/routes/product');
-const orderRoutes = require('./api/routes/orders');
-const bodyParser=require('body-parser')
+// const morgan = require('morgan')
+// const productRoutes = require('./api/routes/product');
+// const orderRoutes = require('./api/routes/orders');
+// const bodyParser=require('body-parser')
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({
